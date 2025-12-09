e8e9fa9c29f1c46e9f783abca8ddc80b401aa7db
Jeel <shahjeel459@gmail.com>
2025-12-09 19:47:22 +0530

    till section 3

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Footer from "./components/footer/Footer";
+import Header from "./components/Header";
+import React from "react";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Home />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
